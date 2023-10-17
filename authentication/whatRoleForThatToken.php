5267bb6447c42fc415b5b401f17f37561d433c5e
vicous6 <vicous@free.fr>
2023-10-17 14:55:16 +0200

    fix

--- authentication/whatRoleForThatToken.php
+++ authentication/whatRoleForThatToken.php
@@ -3,7 +3,7 @@ function whatRoleForThatToken($token)
 {
 
     $user = json_decode(getUserByToken($token));
-    var_dump($user);
+    // var_dump($user);
     $role = $user[0]->role;
 
     return $role;
