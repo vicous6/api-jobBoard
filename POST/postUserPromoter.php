2183c120f8e1de3bb553d6929a98d9fc255e0002
vicous6 <vicous@free.fr>
2023-10-17 21:39:02 +0200

    fix login

--- POST/postUserPromoter.php
+++ POST/postUserPromoter.php
@@ -9,19 +9,17 @@ function createUserPromoter()
     $query = $dbh->prepare("INSERT INTO 
     user (id, username,password,role,email,first_name,last_name,phone,enterprise_id) 
     VALUES (null, :username, :password, :role, :email, :first_name,:last_name,:phone,:enterprise_id)");
-    // var_dump();
-    // if ($post["enterprise_id"] == "") {
-    //     $post["enterprise_id"] = null;
-    // }
-    // if ($post["token"] == "") {
-    //     $post["token"] = null;
-    // }
-    // var_dump($post);
+    
+    $pass= generateToken();
+    $email1=generateToken();
+    $email2=generateToken();
+    $email1.=$email2;
+    var_dump($email1);
     $parameters = [
-        "username" => $post["username"],
-        "password" => cryptPassword($post["password"]),
-        "role" => "user",
-        "email" => $post["email"],
+        "username" => generateToken(),
+        "password" => cryptPassword($pass),
+        "role" => "promoter",
+        "email" => ,
         "first_name" => $post["first_name"],
         "last_name" => $post["last_name"],
         "phone" => $post["phone"],
