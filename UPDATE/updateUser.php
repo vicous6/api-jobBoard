8d5d79397fa43f3dea38723fc0e3402643c962c0
vicous6 <vicous@free.fr>
2023-10-19 12:24:13 +0200

    bug fix dans delete

--- UPDATE/updateUser.php
+++ UPDATE/updateUser.php
@@ -3,19 +3,18 @@
 
 function updateUser($post)
 {
-
+    var_dump(json_encode($post));
     $info = getDatabaseInfo();
     $dbh = new PDO('mysql:host=' . $info["host"] . ';dbname=' . $info["db_name"], $info["user"], $info["password"]);
 
     $query = $dbh->prepare("UPDATE 
     user SET 
     username=:username,
-    
     email=:email,
     first_name=:first_name,
     last_name=:last_name,
     phone=:phone
-       WHERE id=:id"
+    WHERE id=:id"
     );
 
     $parameters = [
