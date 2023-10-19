9ccd5a2a33908424d7763baf4b8b0ed5d72b2443
vicous6 <vicous@free.fr>
2023-10-19 09:58:42 +0200

    bug fix dans delete

--- POST/postJob.php
+++ POST/postJob.php
@@ -7,18 +7,14 @@ function createJob($post)
     $dbh = new PDO('mysql:host=' . $info["host"] . ';dbname=' . $info["db_name"], $info["user"], $info["password"]);
 
     $query = $dbh->prepare("INSERT INTO 
-    job (id, name,descriptionworking_time,location,enterprise_id) 
+    job (id, name,description,working_time,location,enterprise_id) 
     VALUES (null, :name, :description,:working_time,:location,:enterprise_id)");
 
     $parameters = [
         "name" => $post["name"],
         "description" => $post["description"],
-        // "status" => $post["status"],
-        // "workplace" => $post["workplace"],
-        // "wages" => $post["wages"],
         "working_time" => $post["working_time"],
         "location" => $post["location"],
-        // "short_description" => $post["short_description"],
         "enterprise_id" => $post["enterprise_id"]
 
     ];
